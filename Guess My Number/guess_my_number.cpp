df95165f3af25799909ec004578f6289768c887c
Vasmart24 <vasmart24/@yandex.ru>
2023-03-11 22:38:33 +0300

    add logic to second game type. fix some logic.

--- Guess My Number/guess_my_number.cpp
+++ Guess My Number/guess_my_number.cpp
@@ -2,50 +2,46 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-int main() {
-	int maxNumber;
-        int tries = 0;
-        int guessNumber;
-	int typeNumber;
-        cout << "\tWelcome to Guess My Number\n\n";
-	int choiseNumber;
-	cout << "Choose a game type\nYou guess - 1\nAI guess - 2 ";
-	cin >> choiseNumber;
-	cout << "Choose a max number: ";
-	cin >> maxNumber;
-if(choiseNumber == 1) {
-	srand(static_cast<unsigned int>(time(0)));
-        int secretNumber = rand() % maxNumber + 1;
-		do {
-    	cout << "Enter a guess: ";
-    	cin >> guessNumber;
-    	++tries;
-    	if (guessNumber > secretNumber) {
-      		cout << "Too high!\n\n";
-    	}
-	 else if (guessNumber < secretNumber) {
-      		cout << "Too low!\n\n";
- 	}
-	 else {
-		cout << "That's it! You got it in " << tries << " tries!\n";
-  }
-	 while (guessNumber != secretNumber);
-		return 0;
-}
-else if(choiseNumber == 2) {
-	do {
-		cout << "Type a number from 1 to " << maxNumber;
-		cin >> typeNumber;
-		++tries;
-		cout << "Current number " << guessNumber;
-		if(guessNumber < typeNumber) {
-			guessNumber = maxNumber - guessNumber;
-}
-}
-}
-else {
-		cout << "Choosen incorrect number";
-}
-return 0;
-}
 
+int main() {
+  char isCorrectAnswer;
+  int maxNumber;
+  int tries = 0;
+  int guessNumber;
+  cout << "\tWelcome to Guess My Number\n\n";
+  cout << "Choose a game type:\n"
+          << "You guess - 1\n"
+          << "AI guess - 2\n: ";
+  int chosenGameType;
+  cin >> chosenGameType;
+  cout << "Choose a max number: ";
+  cin >> maxNumber;
+  if (chosenGameType == 1) {
+    srand(static_cast<unsigned int>(time(0)));
+    int secretNumber = rand() % maxNumber + 1;
+    do {
+        cout << "Enter a guess: ";
+        cin >> guessNumber;
+        ++tries;
+
+        if (guessNumber > secretNumber) {
+          cout << "Too high!\n\n";
+        } else if (guessNumber < secretNumber) {
+          cout << "Too low!\n\n";
+        } else {
+          cout << "That's it! You got it in " << tries << " tries!\n";
+        }
+        return 0;
+    } while (guessNumber != secretNumber);
+  } else if (chosenGameType == 2) {
+    int firstTry = maxNumber / 2;
+    cout << "Is this your number?" << firstTry;
+    cout << "Write 'yes' if it is :).\n"
+    << "if it's too high write 'too high'\n"
+    << "else write 'too low'.";
+    cin >> isCorrectAnswer;
+    do {
+
+    } while (isCorrectAnswer != 'yes');
+  }
+};
