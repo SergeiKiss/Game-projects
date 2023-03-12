5500e83391c2cb77315ec89beb919be51b20f2e1
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-03-12 14:17:16 +0300

    fix guess_my_number

--- Guess My Number/guess_my_number.cpp
+++ Guess My Number/guess_my_number.cpp
@@ -45,30 +45,35 @@ int main() {
     int rangeMax = maxNumber;
     int rangeMin = 0;
     int currentAIGuess = maxNumber / 2;
+
     cout << "You need to think of a number and answer to AI if it is low or high.\n\n";
     cout << "Is this your number: " << currentAIGuess << "?";
     cout << " Write 'y' if it is :).\n"
-      << "if it's too high, write 'h', "
+      << "If it's too high, write 'h', "
       << "else write 'l'.\n";
+
     cout << "Type here: ";
     cin >> isCorrectAnswer;
+    ++tries;
     do {
       if (isCorrectAnswer == 'h') {
+        ++tries;
         rangeMax = currentAIGuess;
         currentAIGuess = rangeMax - (rangeMax - rangeMin) / 2;
-        cout << "is this your number: " << currentAIGuess << "?\n"
+        cout << "Is this your number: " << currentAIGuess << "?\n"
         << "Type here: ";
         cin >> isCorrectAnswer;
       }
       else if (isCorrectAnswer == 'l') {
+        ++tries;
         rangeMin = currentAIGuess;
         currentAIGuess = rangeMax - (rangeMax - rangeMin) / 2;
-        cout << "is this your number: " << currentAIGuess << "?\n"
+        cout << "Is this your number: " << currentAIGuess << "?\n"
              << "Type here: ";
         cin >> isCorrectAnswer;
       }
     } while (isCorrectAnswer != 'y');
-    cout << "Congratulations! The game is over!";
+    cout << "Congratulations! The game is over with " << tries << " tries.\n";
   }
   return 0;
 };
