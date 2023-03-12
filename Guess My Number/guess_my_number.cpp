7175919cfb130a3c9d444b7c8ad5f0fb85761741
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-03-12 12:24:18 +0300

    fix first type in Guess_My_Number

--- Guess My Number/guess_my_number.cpp
+++ Guess My Number/guess_my_number.cpp
@@ -20,13 +20,14 @@ int main() {
   cout << "Choose a max number: ";
   cin >> maxNumber;
 
+  cout << "\nThe game starts!\n";
+
   if (chosenGameType == 1) {
     srand(static_cast<unsigned int>(time(0)));
     int secretNumber = rand() % maxNumber + 1;
     int guessNumber;
+    cout << "You need to name the hidden number. The amount of attempts is not limited.\n\n";
     do {
-        cout << "\nThe game starts!\n";
-        cout << "You need to name the hidden number. The amount of attempts is not limited.\n";
         cout << "Enter a guess: ";
         cin >> guessNumber;
         ++tries;
@@ -38,17 +39,17 @@ int main() {
         } else {
           cout << "That's it! You got it in " << tries << " tries!\n";
         }
-        return 0;
     } while (guessNumber != secretNumber);
-  } else if (chosenGameType == 2) {
-    int firstTry = maxNumber / 2;
-    cout << "Is this your number?" << firstTry;
-    cout << "Write 'yes' if it is :).\n"
-    << "if it's too high write 'too high'\n"
-    << "else write 'too low'.";
-    cin >> isCorrectAnswer;
-    do {
-
-    } while (isCorrectAnswer != 'yes');
-  }
+  } 
+  // else if (chosenGameType == 2) {
+  //   int firstTry = maxNumber / 2;
+  //   cout << "Is this your number?" << firstTry;
+  //   cout << "Write 'yes' if it is :).\n"
+  //   << "if it's too high write 'too high'\n"
+  //   << "else write 'too low'.";
+  //   cin >> isCorrectAnswer;
+  //   do {
+  //   } while (isCorrectAnswer != "y");
+  // }
+  return 0;
 };
