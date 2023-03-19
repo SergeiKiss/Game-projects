66e5ebd3ab9b366d997d9506ea055749c945df21
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-03-19 16:12:06 +0300

    add game logic

--- Hangman/hangman.cpp
+++ Hangman/hangman.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-//#include <ctime>
 #include <random>
 #include <cctype>
 
@@ -11,7 +10,7 @@ using namespace std;
 int main() {
   const int MAX_WRONG = 8;
   vector<string>words;
-  words.push_back( "GUESS");
+  words.push_back("GUESS");
   words.push_back("HANGMAN");
   words.push_back("DIFFICULT");
   random_device rng;
@@ -21,16 +20,35 @@ int main() {
   int wrong = 0;
   string soFar(THE_WORD.size(), '-');
   string used = "";
+
   cout << "Welcome to Hangman. Good luck!\n";
   while ((wrong < MAX_WRONG) && (soFar != THE_WORD)) {
     cout << "\n\nYou have " << (MAX_WRONG - wrong) << " incorrect guesses left.\n";
     cout << "\nYou've used the following letters:\n" << used << endl;
-    cout << "\nSo far. thr word is:\n" << soFar << endl;
-    //
-    //
-    //
-    //
-    //
+    cout << "\nSo far, the word is:\n" << soFar << endl;
+    char guess;
+    cout << "\n\nEnter your guess: ";
+    cin >> guess;
+    guess = toupper(guess);
+    while (used.find(guess) != string::npos) {
+      cout << "\nYou've already guessed " << guess << endl;
+      cout << "Enter your guess: ";
+      cin >> guess;
+      guess = toupper(guess);
+    }
+    used += guess;
+    if (THE_WORD.find(guess) != string::npos) {
+      cout << "That's right! " << guess << " is in the word.\n";
+      for (int i = 0; i < THE_WORD.length(); ++i) {
+        if (THE_WORD[i] == guess) {
+          soFar[i] = guess;
+        }
+      }
+    } else {
+      cout << "Sorry, " << guess << " isn't in the word.\n";
+      ++wrong;
+    }
+
     if (wrong == MAX_WRONG) {
       cout << "\nYou've een hanged!";
     }
