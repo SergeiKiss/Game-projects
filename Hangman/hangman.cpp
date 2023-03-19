6985a5842c2afbd3772fe3175e1bb76d1bd35707
Vasmart24 <vasmart24/@yandex.ru>
2023-03-19 15:58:26 +0300

    add end and begin cycle to hangman.cpp

--- Hangman/hangman.cpp
+++ Hangman/hangman.cpp
@@ -9,17 +9,35 @@
 using namespace std;
 
 int main() {
-    const int MAX_WRONG = 8;
-    vector<string>words;
-    words.push_back( "GUESS");
-    words.push_back("HANGMAN");
-    words.push_back("DIFFICULT");
-    random_device rng;
-    mt19937 urng(rng());
-    shuffle(words.begin(), words.end(), urng);
-    const string THE_WORD = words[0];
-    int wrong = 0;
-    string soFar(THE_WORD.size(), '-');
-    string used = "";
-    cout << "Welcome to Hangman. Good luck!\n";
+  const int MAX_WRONG = 8;
+  vector<string>words;
+  words.push_back( "GUESS");
+  words.push_back("HANGMAN");
+  words.push_back("DIFFICULT");
+  random_device rng;
+  mt19937 urng(rng());
+  shuffle(words.begin(), words.end(), urng);
+  const string THE_WORD = words[0];
+  int wrong = 0;
+  string soFar(THE_WORD.size(), '-');
+  string used = "";
+  cout << "Welcome to Hangman. Good luck!\n";
+  while ((wrong < MAX_WRONG) && (soFar != THE_WORD)) {
+    cout << "\n\nYou have " << (MAX_WRONG - wrong) << " incorrect guesses left.\n";
+    cout << "\nYou've used the following letters:\n" << used << endl;
+    cout << "\nSo far. thr word is:\n" << soFar << endl;
+    //
+    //
+    //
+    //
+    //
+    if (wrong == MAX_WRONG) {
+      cout << "\nYou've een hanged!";
+    }
+    else {
+      cout << "\nYou guessed it!";
+    }
+    cout << "\nThe word was " << THE_WORD << endl;
+    return 0;
+  }
 }
