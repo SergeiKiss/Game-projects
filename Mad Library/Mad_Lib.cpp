0ba796d9535e96182bcb0c26fc82a8804fdf3fc1
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-04-02 12:49:04 +0300

    add binary files for linux

--- Mad Library/Mad_Lib.cpp
+++ Mad Library/Mad_Lib.cpp
@@ -9,10 +9,10 @@ void tellStory(string name, string noun, int number, string bodyPart, string ver
 int main() {
   cout << "Welcome to Mad Lib.\n\n"
        << "Answer the following questions to help create a new story.\n";
-  string name = askText("please enter a name: ");
-  string noun = askText("please, enter a plural noun: ");
+  string name = askText("Please enter a name: ");
+  string noun = askText("Please, enter a plural noun: ");
   int number = askNumber("Please enter a number: ");
-  string bodyPart = askText("Please enter a body part");
+  string bodyPart = askText("Please enter a body part: ");
   string verb = askText("Please enter a verb: ");
   tellStory(name, noun, number, bodyPart, verb);
 }
